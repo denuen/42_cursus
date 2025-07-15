eadb19b93a52b90287ac236eb7608932b5d7760d
denuen <anto.pintauro1@gmail.com>
2025-07-15 23:24:53 +0200

    fix: fixed mariadb execution

---
+++ inception/srcs/services/mariadb/scripts/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+envsubst < /tmp/init.sql.template > /tmp/init.sql
+
+# Initialize database only if it not exists
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm
+fi
+
+# Starts mysql in background
+mysqld_safe --datadir=/var/lib/mysql --nowatch &
+
+# Wait mysql start
+attempt_counter=0
+max_attempts=30
+until mysqladmin ping --silent; do
+	if [ ${attempt_counter} -eq ${max_attempts} ]; then
+		echo "Error: Could not connect to MySQL."
+		exit 1
+	fi
+	attempt_counter=$((attempt_counter+1))
+	sleep 1
+done
+
+mysql < /tmp/init.sql
+
+mysqladmin shutdown
+
+# Start MySQL in foreground
+exec mysqld_safe --datadir=/var/lib/mysql
