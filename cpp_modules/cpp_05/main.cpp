9a8b251a2914c9431f896680d7b4ed5c6a77bdf0
denuen <anto.pintauro1@gmail.com>
2025-07-29 15:19:57 +0200

    feat: added basic tests

---
+++ cpp_modules/cpp_05/main.cpp
@@ -0,0 +1,23 @@
+#include "Bureaucrat.hpp"
+
+
+int	main() {
+
+	Bureaucrat	noargs;
+
+	try {
+		Bureaucrat	validArgs("John", 12);
+		Bureaucrat	invalidArgs("John", 151);
+
+		std::cout << validArgs;
+		std::cout << invalidArgs;
+	} catch(const Bureaucrat::GradeTooHighException& e) {
+		std::cerr << "Caught GradeTooHighException: " << e.what() << std::endl;
+	}
+	catch(const Bureaucrat::GradeTooLowException& e) {
+		std::cerr << "Caught GradeTooLowException: " << e.what() << std::endl;
+	}
+
+	std::cout << noargs;
+
+}
