490c3752f0712510f88252da7039f51d681d8a97
denuen <anto.pintauro1@gmail.com>
2025-07-29 15:21:18 +0200

    feat: moved exercise into appropriate directory

---
+++ cpp_modules/cpp_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,55 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string& name, const int grade) :
+name(name), grade(grade) {
+
+	if (grade < HIGHEST_GRADE_RATE) {
+		throw GradeTooHighException();
+	} else if (grade > LOWEST_GRADE_RATE) {
+		throw GradeTooLowException();
+	}
+
+}
+
+const char*	Bureaucrat::GradeTooLowException::what() const throw() {
+
+	return ("Error: Bureaucrat: grade too low!");
+
+}
+
+const char*	Bureaucrat::GradeTooHighException::what() const throw() {
+
+	return ("Error: Bureaucrat: grade too high!");
+
+}
+
+void	Bureaucrat::incrementGrade() {
+
+	if ((grade - 1) < HIGHEST_GRADE_RATE) {
+		throw GradeTooHighException();
+	} else {
+		grade -= 1;
+	}
+
+}
+
+void	Bureaucrat::decrementGrade() {
+
+	if ((grade + 1) > LOWEST_GRADE_RATE) {
+		throw GradeTooLowException();
+	} else {
+		grade += 1;
+	}
+
+}
+
+Bureaucrat::~Bureaucrat() {
+
+}
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& b) {
+
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+	return (os);
+}
+
