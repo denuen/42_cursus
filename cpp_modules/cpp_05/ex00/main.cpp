fc23f83f89cb5ecf8a939b16df1a142cd4ef1a46
denuen <anto.pintauro1@gmail.com>
2025-07-29 17:33:16 +0200

    feat: updated main.cpp tests. Updated tests to ensure that all the try-catch blocks handle correctly all the exceptions

--- cpp_modules/cpp_05/ex00/main.cpp
+++ cpp_modules/cpp_05/ex00/main.cpp
@@ -3,17 +3,28 @@
 
 int	main() {
 
+	// valid
 	try {
 		Bureaucrat	validArgs("John", 12);
-		Bureaucrat	invalidArgs("John", 151);
+		std::cout << validArgs << std::endl;
+	} catch(const std::exception& e) {
+		std::cerr << "Caught exception: " << e.what() << std::endl;
+	}
 
-		std::cout << validArgs;
-		std::cout << invalidArgs;
+	// invalid
+	try {
+		Bureaucrat	invalidArgs("John", 151);
+		std::cout << invalidArgs << std::endl;
+	} catch(const Bureaucrat::GradeTooLowException& e) {
+		std::cerr << "Caught GradeTooLowException: " << e.what() << std::endl;
+	}
+
+	// invalid
+	try {
+		Bureaucrat	invalidArgs2("Jane", 0);
+		std::cout << invalidArgs2 << std::endl;
 	} catch(const Bureaucrat::GradeTooHighException& e) {
 		std::cerr << "Caught GradeTooHighException: " << e.what() << std::endl;
-	}
-	catch(const Bureaucrat::GradeTooLowException& e) {
-		std::cerr << "Caught GradeTooLowException: " << e.what() << std::endl;
 	}
 
 }
