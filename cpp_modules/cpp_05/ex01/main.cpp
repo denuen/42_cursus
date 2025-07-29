cb833cf4fd699e29b6e3a342ff310178a87f2fc6
denuen <anto.pintauro1@gmail.com>
2025-07-29 17:30:20 +0200

    feat: added several tests to ensure the exceptions are handled correctly

---
+++ cpp_modules/cpp_05/ex01/main.cpp
@@ -0,0 +1,77 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include <iostream>
+
+int main() {
+
+	try {
+		Bureaucrat	alice("Alice", 1);
+		Bureaucrat	bob("Bob", 150);
+		Bureaucrat	charlie("Charlie", 75);
+
+		Form	taxForm("Tax Declaration", 50, 30);
+		Form	leaveForm("Leave Request", 150, 100);
+		Form	secretForm("Top Secret", 1, 1);
+
+		std::cout << alice << std::endl;
+		std::cout << bob << std::endl;
+		std::cout << charlie << std::endl;
+		std::cout << std::endl;
+
+		std::cout << taxForm << std::endl;
+		std::cout << leaveForm << std::endl;
+		std::cout << secretForm << std::endl;
+		std::cout << std::endl;
+
+		alice.signForm(secretForm);		// valid
+		std::cout << secretForm << std::endl;
+		std::cout << std::endl;
+
+		bob.signForm(leaveForm);		// vaild
+		std::cout << leaveForm << std::endl;
+		std::cout << std::endl;
+
+		bob.signForm(taxForm);			// invalid
+		std::cout << taxForm << std::endl;
+		std::cout << std::endl;
+
+		charlie.signForm(taxForm);		// invalid
+		std::cout << taxForm << std::endl;
+		std::cout << std::endl;
+
+		charlie.signForm(secretForm);	// invalid
+		std::cout << secretForm << std::endl;
+		std::cout << std::endl;
+
+		try {
+			Form	invalidForm1("Invalid High", 0, 50);
+		} catch (const Form::GradeTooHighException& e) {
+			std::cout << "Caught exception for grade 0: " << e.what() << std::endl;
+		}
+
+		try {
+			Form	invalidForm2("Invalid Low", 50, 151);
+		} catch (const Form::GradeTooLowException& e) {
+			std::cout << "Caught exception for grade 151: " << e.what() << std::endl;
+		}
+
+		// It catches the first caught exception
+		try {
+			Form	invalidForm3("Invalid Form", 0, 151);
+		} catch (const Form::GradeTooHighException& e) {
+			std::cout << "Caught exception for grade 0: " << e.what() << std::endl;
+		}
+		 catch (const Form::GradeTooLowException& e) {
+			std::cout << "Caught exception for grade 151: " << e.what() << std::endl;
+		}
+
+	}
+	catch (const Form::GradeTooHighException& e) {
+		std::cout << "Caught exception for grade 0: " << e.what() << std::endl;
+	}
+	 catch (const Form::GradeTooLowException& e) {
+		std::cout << "Caught exception for grade 151: " << e.what() << std::endl;
+	}
+
+	return 0;
+}
