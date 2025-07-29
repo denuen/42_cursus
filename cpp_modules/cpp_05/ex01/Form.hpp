2f51b3b281a83719b70ebe3486c2abc6d7161c09
denuen <anto.pintauro1@gmail.com>
2025-07-29 17:29:30 +0200

    feat: added Form header

---
+++ cpp_modules/cpp_05/ex01/Form.hpp
@@ -0,0 +1,45 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat;
+
+class Form {
+
+	private:
+		const std::string	name;
+		bool				isSigned;
+		const int			requiredSignGrade;
+		const int			requiredExecuteGrade;
+
+		Form();
+		Form(const Form& f);
+		Form&	operator=(const Form& f);
+	public:
+		Form(const std::string& name, const int signGrade, const int executeGrade);
+
+		inline std::string	getName() const { return (name); }
+		inline bool			getIsSigned() const { return (isSigned); }
+		inline int			getRequiredSignGrade() const { return (requiredSignGrade); }
+		inline int			getRequiredExecuteGrade() const { return (requiredExecuteGrade); }
+
+		void				beSigned(const Bureaucrat& b);
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		~Form();
+};
+
+std::ostream&	operator<<(std::ostream& os, const Form& f);
+
+#endif
