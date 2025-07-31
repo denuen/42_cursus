2ea76841a5dcda0f56248cbcf87ca446c98711a9
denuen <anto.pintauro1@gmail.com>
2025-07-31 17:12:23 +0200

    feat: added forms that will interact with the Bureaucrat

---
+++ cpp_modules/cpp_05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,53 @@
+#include "RobotomyRequestForm.hpp"
+#include "Bureaucrat.hpp"
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string& target, const std::string& formName) :
+AForm(formName, REQUIRED_SIGN_GRADE, REQUIRED_EXEC_GRADE), target(target), isTargetPresent(false) {
+
+	if (target.size() == 0) {
+		throw std::invalid_argument ("Error: filename must be a non-null string.");
+		return ;
+	}
+
+	std::ifstream	test;
+
+	test.open(target);
+	if (test.good()) {
+		isTargetPresent = true;
+	}
+	test.close();
+
+}
+
+void	RobotomyRequestForm::execute(Bureaucrat const& b) const {
+
+	if (!this->getIsSigned()) {
+		throw NotSignedException();
+	}
+	if (b.getGrade() > REQUIRED_EXEC_GRADE) {
+		throw GradeTooLowException();
+	}
+
+	std::cout << "*******ROBOTIMIZATION REQUESTED*******" << std::endl;
+
+	if (isTargetPresent) {
+		std::cout << "Target: " << target << " is being robotimized..." << std::endl;
+
+		bool isRobotimized = std::rand() % 2;
+
+		if (isRobotimized) {
+			std::cerr << "Error: robotomization failed due to internal errors. Please retry." << std::endl;
+		} else {
+			std::cout << "Target: " << target << " has been successfully robotomized." << std::endl;
+		}
+	} else {
+		std::cerr << "Error: target: " << target << " cannot be robotomized. Ensure the file exists." << std::endl;
+	}
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {
+
+}
---
+++ cpp_modules/cpp_05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,31 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm {
+
+	private:
+		const std::string			target;
+
+		bool						isTargetPresent;
+
+		static const int			REQUIRED_SIGN_GRADE = 72;
+		static const int			REQUIRED_EXEC_GRADE = 45;
+
+		RobotomyRequestForm();
+		RobotomyRequestForm(const RobotomyRequestForm& r);
+
+	public:
+		RobotomyRequestForm(const std::string& target, const std::string& formName);
+
+		inline int		getRequiredSignGrade() const { return (REQUIRED_SIGN_GRADE); }
+		inline int		getRequiredExecGrade() const { return(REQUIRED_EXEC_GRADE); }
+
+		virtual void	execute(Bureaucrat const& b) const;
+
+		~RobotomyRequestForm();
+};
+
+
+#endif
