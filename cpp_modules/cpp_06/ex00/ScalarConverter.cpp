ea97632ccb016272cd3fa94ce9798323de174033
denuen <anto.pintauro1@gmail.com>
2025-08-13 12:44:26 +0200

    feat: added ScalarConverter.cpp [cpp_06, ex00].

---
+++ cpp_modules/cpp_06/ex00/ScalarConverter.cpp
@@ -0,0 +1,125 @@
+#include "ScalarConverter.hpp"
+#include <cstdlib>
+#include <sstream>
+#include <iomanip>
+
+void	ScalarConverter::convert(const std::string& literal) {
+
+	if (literal.size() == 0) {
+		displayConversionResult("impossibile", "impossibile", "impossibile", "impossibile");
+	} else {
+		std::string	toDisplay = literal;
+		char*	endptr;
+
+		if (toDisplay[toDisplay.length() - 1] == 'f')
+			toDisplay = toDisplay.substr(0, toDisplay.length() - 1);
+
+		long long	rInt = std::strtoll(toDisplay.c_str(), &endptr, 10);
+		if (!endptr || endptr == toDisplay.c_str()) {
+			rInt = std::numeric_limits<long long>::max();
+		}
+
+		displayConversionResult(checkCharBounds(rInt), checkIntBounds(rInt),
+								checkFloatBounds(literal, endptr), checkDoubleBounds(literal, endptr));
+	}
+
+}
+
+void	displayConversionResult(const std::string& rChar, const std::string& rInt,
+								const std::string& rFloat, const std::string& rDouble) {
+
+		std::cout << "char: " << rChar << std::endl;
+		std::cout << "int: " << rInt << std::endl;
+		std::cout << "float: " << rFloat << std::endl;
+		std::cout << "double: " << rDouble << std::endl;
+
+}
+
+std::string	checkCharBounds(long long rChar) {
+
+	std::string	res;
+
+	if (rChar >= 0 && rChar <= 31 || rChar == 127) {
+		res = "Non displayable";
+	} else if (rChar < 0 || rChar > 127) {
+		res = "impossible";
+	} else {
+		res = std::string(1, static_cast<char>(rChar));
+	}
+
+	return (res);
+}
+
+std::string	checkIntBounds(long long rInt) {
+
+	std::string			res;
+
+	if (rInt > std::numeric_limits<int>::max() || rInt < std::numeric_limits<int>::min()) {
+		res = "impossible";
+	} else {
+		res = std::to_string(rInt);
+	}
+
+	return (res);
+}
+
+std::string checkFloatBounds(const std::string& literal, char* endptr) {
+
+	if (literal == "inf" || literal == "+inf" || literal == "inff" || literal == "+inff") {
+		return "+inff";
+	} else if (literal == "-inf" || literal == "-inff") {
+		return "-inff";
+	}
+
+	std::string	normalizedLiteral;
+	long double	rFloat;
+
+	normalizedLiteral = literal.substr(0, literal.find_first_of('f'));
+	rFloat = std::strtold(normalizedLiteral.c_str(), &endptr);
+
+	if (endptr == normalizedLiteral.c_str()) {
+		return ("impossible");
+	} else if (std::isnan(rFloat)) {
+		return ("nanf");
+	} else if (rFloat > std::numeric_limits<float>::max()
+		|| rFloat < std::numeric_limits<float>::min()) {
+			return ("impossible");
+	}
+
+	if (normalizedLiteral.find('.') == std::string::npos && normalizedLiteral.find('e') == std::string::npos) {
+		normalizedLiteral += ".0";
+	}
+
+	return (normalizedLiteral + 'f');
+}
+
+std::string checkDoubleBounds(const std::string& literal, char* endptr) {
+
+	if (literal == "inf" || literal == "+inf" || literal == "inff" || literal == "+inff") {
+		return ("+inf");
+	} else if (literal == "-inf" || literal == "-inff") {
+		return ("-inf");
+	}
+
+	std::string	normalizedLiteral;
+	long double	rDouble;
+
+	normalizedLiteral = literal.substr(0, literal.find_first_of('f'));
+	rDouble = std::strtold(normalizedLiteral.c_str(), &endptr);
+
+	if (endptr == normalizedLiteral.c_str()) {
+			return ("impossible");
+	} else if (std::isnan(rDouble)) {
+		return ("nan");
+	} else if (rDouble > std::numeric_limits<double>::max()
+		|| rDouble < std::numeric_limits<double>::min()) {
+			return ("impossible");
+	}
+
+	if (normalizedLiteral.find('.') == std::string::npos && normalizedLiteral.find('e') == std::string::npos) {
+		normalizedLiteral += ".0";
+	}
+
+	return (normalizedLiteral);
+
+}
