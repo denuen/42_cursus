b2bf686e006e056a1dac45fdbef25035b39176c6
denuen <anto.pintauro1@gmail.com>
2025-08-14 09:46:08 +0200

    feat: added Array.hpp template class [cpp_07, ex02]

---
+++ cpp_modules/cpp_07/ex02/Array.hpp
@@ -0,0 +1,30 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template <typename T>
+class Array {
+
+	private:
+		T*				buffer;
+		unsigned int	n;
+
+	public:
+		Array();
+		Array(const unsigned int n);
+		Array(const Array& a);
+
+		Array&		operator=(const Array& a);
+		T&			operator[](unsigned int index);
+		const T&	operator[](unsigned int index) const;
+
+		inline unsigned int	size() const { return (n); }
+
+		class	OutOfBounds : public std::exception {
+			public:
+				virtual const char*	what() const throw();
+		};
+
+		~Array();
+};
+
+#endif
