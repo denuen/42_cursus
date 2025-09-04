e861c82952ef357b0108d61df7770c32f90b9595
denuen <anto.pintauro1@gmail.com>
2025-09-04 09:56:46 +0200

    fix: fixed includes errors, omitted by macOS compiler [cpp_08, ex01]

--- cpp_modules/cpp_08/ex01/Span.cpp
+++ cpp_modules/cpp_08/ex01/Span.cpp
@@ -6,33 +6,30 @@
 /*   By: apintaur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 12:19:35 by apintaur          #+#    #+#             */
-/*   Updated: 2025/08/14 12:19:36 by apintaur         ###   ########.fr       */
+/*   Updated: 2025/09/04 09:56:03 by apintaur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <algorithm>
+#include <limits>
 
-Span::Span(const unsigned int N) :
-n(N) {
+Span::Span(const unsigned int N) : n(N) {
 
 	if (n > 0) {
 		vec.reserve(n);
 	}
-
 }
 
-Span::Span(const Span& s):
-n(s.n) {
+Span::Span(const Span& s) : n(s.n) {
 
 	if (n > 0) {
 		vec.reserve(n);
 		vec = s.vec;
 	}
-
 }
 
-Span&	Span::operator=(const Span& s) {
+Span& Span::operator=(const Span& s) {
 
 	if (this != &s) {
 		vec.clear();
@@ -46,17 +43,16 @@ Span&	Span::operator=(const Span& s) {
 	return (*this);
 }
 
-void	Span::addNumber(const int nbr) {
+void Span::addNumber(const int nbr) {
 
 	if (vec.size() >= n) {
 		throw SpanIsFull();
 	} else {
 		vec.push_back(nbr);
 	}
-
 }
 
-void	Span::addMultipleNumbers(unsigned int start, unsigned int end) {
+void Span::addMultipleNumbers(unsigned int start, unsigned int end) {
 
 	if (start > end) {
 		throw BadRange();
@@ -65,21 +61,20 @@ void	Span::addMultipleNumbers(unsigned int start, unsigned int end) {
 		if (vec.size() == n) {
 			throw SpanIsFull();
 		}
-		vec.emplace_back(start);
+		vec.push_back(start);
 		start++;
 	}
-
 }
 
-int	Span::shortestSpan() const {
+int Span::shortestSpan() const {
 
-	int					diff = std::numeric_limits<int>::max();
+	int diff = std::numeric_limits<int>::max();
 
 	if (vec.size() <= 1) {
 		throw SpanIsEmpty();
 	} else {
-		std::vector<int>	tmp = vec;
-		int					size = vec.size();
+		std::vector<int> tmp  = vec;
+		int				 size = vec.size();
 
 		std::sort(tmp.begin(), tmp.end());
 		for (int i = 0; i < size - 1; i++) {
@@ -91,7 +86,7 @@ int	Span::shortestSpan() const {
 	return (diff);
 }
 
-int	Span::longestSpan() const {
+int Span::longestSpan() const {
 
 	if (vec.size() <= 1) {
 		throw SpanIsEmpty();
@@ -99,24 +94,25 @@ int	Span::longestSpan() const {
 		// std::vector<int>	tmp = vec;
 		// std::sort(tmp.begin(), tmp.end());
 		// return (tmp.back() - tmp.front());
-		std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> minMax =
-													std::minmax_element(vec.begin(), vec.end());
-		return (*minMax.second - *minMax.first);
+		std::vector<int>::const_iterator minIt =
+			std::min_element(vec.begin(), vec.end());
+		std::vector<int>::const_iterator maxIt =
+			std::max_element(vec.begin(), vec.end());
+		return (*maxIt - *minIt);
 	}
 }
 
-const char*	Span::SpanIsFull::what() const throw() {
+const char* Span::SpanIsFull::what() const throw() {
 	return ("Span is full!");
 }
 
-const char*	Span::SpanIsEmpty::what() const throw() {
+const char* Span::SpanIsEmpty::what() const throw() {
 	return ("Span is Empty!");
 }
 
-const char*	Span::BadRange::what() const throw() {
+const char* Span::BadRange::what() const throw() {
 	return ("Invalid numbers range!");
 }
 
 Span::~Span() {
-
 }
